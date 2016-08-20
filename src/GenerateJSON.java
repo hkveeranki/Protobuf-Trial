df5e837fe79510620d088ddb4f08bdd2c2715f0c
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2016-08-20 23:41:14 +0530

    Added JSON base files

---
+++ src/GenerateJSON.java
@@ -0,0 +1,18 @@
+
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+//import java.util.Arrays;
+
+/**
+ * Created by harry7 on 20/8/16.
+ */
+public class GenerateJSON {
+    static public void main(String[] args){
+        ArrayList<JSONObject> objects = new ArrayList<>();
+        System.out.println(objects.toString());
+    }
+}
